565e96045de70a58cd013fa64b2e099f04d9fc70
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-04-17 05:42:00 +0000

    add

--- src/main/java/co_templates/DataTypeStrings.java
+++ src/main/java/co_templates/DataTypeStrings.java
@@ -1,35 +1,35 @@
 package co_templates;
 
-public class DataTypeStrings {
-    // public static void main(String[] args){
-    //     String strFirst = "Hello !";
-    //     String strSecond = new String("Hello !");
-    //     // strFirst instanceof String
-    //     // true
-    //     // strSecond instanceof String
-    //     // true
+// public class DataTypeStrings {
+//     // public static void main(String[] args){
+//     //     String strFirst = "Hello !";
+//     //     String strSecond = new String("Hello !");
+//     //     // strFirst instanceof String
+//     //     // true
+//     //     // strSecond instanceof String
+//     //     // true
 
-    //     // strSecond instanceof String
-    //     // true
-    //     // strFirst.length()
-    //     // 7
-    //     // strSecond.length()
-    //     // 7
-    //     // strSecond.concat(strFirst)
-    //     // "Hello !Hello !"
-    //     // strFirst.replaceAll("l", "R")
-    //     // "HeRRo !"
-    //     // System.out.println(strFirst);
-    // }
-
-    public class Main {
-        public static void main(String[] args) {
-          String myStr1 = "Hello";
-          String myStr2 = "Hello";
-          String myStr3 = "Another String";
-          System.out.println(myStr1.equals(myStr2));
-          System.out.println(myStr1.equals(myStr3));
-        }
-      }
+//     //     // strSecond instanceof String
+//     //     // true
+//     //     // strFirst.length()
+//     //     // 7
+//     //     // strSecond.length()
+//     //     // 7
+//     //     // strSecond.concat(strFirst)
+//     //     // "Hello !Hello !"
+//     //     // strFirst.replaceAll("l", "R")
+//     //     // "HeRRo !"
+//     //     // System.out.println(strFirst);
+//     // }
       
-}
+// }
+
+public class DataTypeStrings {
+    public static void main(String[] args) {
+      String myStr1 = "Hello";
+      String myStr2 = "Hello";
+      String myStr3 = "Another String";
+      System.out.println(myStr1.equals(myStr2));
+      System.out.println(myStr1.equals(myStr3));
+    }
+  }

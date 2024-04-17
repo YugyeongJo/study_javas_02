4af8e757a68456321b6a221d91bfd3cef16605b2
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-04-17 06:18:33 +0000

    update

--- src/main/java/co_templates/DataTypeDates.java
+++ src/main/java/co_templates/DataTypeDates.java
@@ -7,7 +7,7 @@ public class DataTypeDates {
     public static void main(String[] args) {  
     LocalDateTime myDateObj = LocalDateTime.now();  
     System.out.println("Before formatting: " + myDateObj);  
-    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  
+    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
     
     String formattedDate = myDateObj.format(myFormatObj);  
     System.out.println("After formatting: " + formattedDate);  
